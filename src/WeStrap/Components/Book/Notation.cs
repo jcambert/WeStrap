800857163f0da1925b1cac17527adb8e24ebce7a
Ambert Jean-Christophe <jc.ambert@free.fr>
2020-06-24 22:57:37 +0200

    add book

---
+++ src/WeStrap/Components/Book/Notation.cs
@@ -0,0 +1,9 @@
+﻿namespace WeStrap
+{
+    public class Notation
+    {
+        public double Note { get; set; }
+        public string Evaluation { get; set; }
+        public int Numbers { get; set; }
+    }
+}
